079be57a39d0ad1b8c21e4f7fc403ae37e5553ed
hye1ee <hyewon0809@kaist.ac.kr>
2023-12-01 18:06:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-function App() {
+const App = () => {
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
+  useEffect(() => {
+    console.log(scroll);
+  }, [scroll]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+    </AppWrapper>
   );
-}
+};
 
 export default App;
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 5000px;
+
+  box-sizing: border-box;
+  padding: 200px 150px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  gap: 50px;
+  overflow: auto;
+`;
+
+const Box = styled.div`
+  width: 100%;
+  height: 500px;
+
+  background-color: gray;
+`;
