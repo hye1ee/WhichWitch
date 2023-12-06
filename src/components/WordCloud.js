1128ab91a03793e3178a63fc3d76d4ee2a606299
hye1ee <hyewon0809@kaist.ac.kr>
2023-12-06 23:19:46 +0900

    add worldcloud

---
+++ src/components/WordCloud.js
@@ -0,0 +1,49 @@
+import React from "react";
+import WordCloud from "react-wordcloud";
+
+// 여자 워드클라우드 데이터
+
+const colors = [
+  "#f3eeff",
+  "#d4bdff",
+  "#B98AFF",
+  "#a24dff",
+  "#8200dd",
+  "#540091",
+  "#2a004d",
+  "#D0FF8A",
+  "#a0d900",
+  "#80af00",
+  "#628700",
+  "#456100",
+  "#2b3d00",
+  "#121d00",
+];
+
+const fontOptions = {
+  // fontFamily: "LeferiPointWhite.ttf", // 폰트 실제경로로 변경해 주시면 됩니다
+  fontWeight: 700,
+};
+
+const getRandomColor = () => {
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const getRandomColorWords = (data) => {
+  return data.map((word) => {
+    return { ...word, color: getRandomColor() };
+  });
+};
+
+// 화면 비율은 바꾸지 말아주세요 (사이즈는 변경하셔도 됩니다)
+// 디폴트로 여성의 워드클라우드를 먼저 그리게 만들어 두었습니다
+const WordCloudGender = (props) => {
+  const randomColorWords = getRandomColorWords(props.data);
+  return (
+    <div style={{ width: "1130px", height: "568px" }}>
+      <WordCloud words={randomColorWords} options={fontOptions} />
+    </div>
+  );
+};
+
+export default WordCloudGender;
--- src/pages/Gender.js
+++ src/pages/Gender.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 import Layout from "../components/Layout";
+import WordCloudGender from "../components/WordCloud";
+import { genderData } from "../assets/genderData";
+import Button from "../components/Button";
+import { useState } from "react";
 
 const Gender = () => {
+  const [female, setFemale] = useState(true);
+  console.log(genderData[female ? "female" : "male"]);
+
   return (
     <Layout
       title="성별 초집중 키워드"
@@ -9,6 +16,12 @@ const Gender = () => {
       description={"각 성별은 이번 분기에 어떤 키워드를 검색할까요?"}
     >
       Hello
+      <Button
+        items={["남자", "여자"]}
+        selectedItem={female ? "여자" : "남자"}
+        selectedHandler={(el) => setFemale(el === "여자")}
+      />
+      <WordCloudGender data={genderData[female ? "female" : "male"]} />
     </Layout>
   );
 };
